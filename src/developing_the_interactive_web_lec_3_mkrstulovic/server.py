f9ba6a1e197091c13b3f8213ce09e824b3b3bf9b
Marko Krstulovic <mk366d@outlook.com>
2026-09-17 09:32:36 -0500

    Guestbook functionality

--- src/developing_the_interactive_web_lec_3_mkrstulovic/server.py
+++ src/developing_the_interactive_web_lec_3_mkrstulovic/server.py
@@ -4,10 +4,20 @@ from flask import request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    global guestbook
+    return render_template("index.html", guestbook=guestbook)
 
-@app.route('/hi', methods=['GET'])
+""" @app.route('/hi', methods=['GET'])
 def hello_world():
   user_name = request.args.get("userName", "unknown")
   return render_template('main.html', user=user_name) 
+ """
+guestbook = []
+
+@app.route('/guestbook', methods=['POST'])
+def add_to_guestbook():
+   global guestbook
+   if request.form.get("name"):
+      guestbook.append(request.form.get("name"))
+   return render_template("index.html", guestbook=guestbook)
